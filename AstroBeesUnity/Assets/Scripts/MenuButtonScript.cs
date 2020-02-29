bfa46e1e188d9936bab78cf3fc4270699134c7f9
sidfoe <sidfoe2125@gmail.com>
2020-02-29 17:00:02 -0600

    main menu updates

---
+++ AstroBeesUnity/Assets/Scripts/MenuButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonScript : MonoBehaviour
+{
+    public GameObject howToPlayPanel;
+    
+    //void OnMouseDown()
+    //{
+    //    Invoke("NextScene", 0.15f);
+    //}
+
+    void NextScene()
+	{
+        SceneManager.LoadScene("IntroCutscene");
+	}
+
+    public void HowToPlayPanelOn()
+    {
+        howToPlayPanel.SetActive(true);
+    }
+
+    public void HowToPlayPanelOff()
+    {
+        howToPlayPanel.SetActive(false);
+    }
+}
